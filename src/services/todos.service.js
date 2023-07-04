0ead0ed8b14f8208ca3e23d0609e72bd86fbafd8
Alexey <butuzz74@gmail.com>
2023-07-04 13:33:05 +0300

    create task

---
+++ src/services/todos.service.js
@@ -0,0 +1,18 @@
+import httpService from "./http.service";
+const todosEndepoint = "todos/";
+const todosService = {
+    fetch: async () => {
+        const { data } = await httpService.get(todosEndepoint, {
+            params: {
+                _page: 1,
+                _limit: 10,
+            },
+        });
+        return data;
+    },
+    create: async (content) => {
+        const { data } = await httpService.post(todosEndepoint, content);        
+        return data;
+    },
+};
+export default todosService;
